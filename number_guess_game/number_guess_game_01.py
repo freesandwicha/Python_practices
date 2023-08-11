37f6a0071f7c93266fcd677b33f3397eee316cc6
SummerDay <100746570+freesandwicha@users.noreply.github.com>
2023-08-11 16:15:13 +1000

    Update number_guess_game_01.py

--- number_guess_game/number_guess_game_01.py
+++ number_guess_game/number_guess_game_01.py
@@ -1,1 +1,41 @@
 
+# -*- coding = utf-8 -*-
+# Worker : HAN XIA
+# Motto : Practice makes perfect.
+# Time : 11/8/2023 3:51 pm
+
+from random import randint
+
+lower_num, higher_num = 1, 10
+random_number: int = randint(lower_num, higher_num)
+
+
+def guess_number():
+    print(f"Guess the name in the range from {lower_num} to {higher_num}")
+    count = 0
+    value = 3
+    while value > 0:
+        try:
+            user_number = int(input("Guess : "))
+
+        except ValueError as e :
+            print("Please enter an integer.")
+            continue
+            #Continue won't execute the data after continue. Go back to the start of while:
+        value -= 1  # 2 1 0
+        count += 1  # 1 2 3
+        if user_number < random_number:
+            print("Your number is lower...")
+        elif user_number > random_number:
+            print("Your number is higher...")
+        else:
+            print("You got it.")
+            break
+            #Break will jump from this loop.
+    else:
+        print("Sorry, you just has three chances.")
+
+
+
+if __name__ == "__main__":
+    guess_number()
