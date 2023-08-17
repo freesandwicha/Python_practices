c7ec0d52340aa2fa8a5a6b6cbdedb903a6d6909a
SummerDay <100746570+freesandwicha@users.noreply.github.com>
2023-08-17 21:08:51 +1000

    Create QR_generator.py

---
+++ QR_generator_07/QR_generator.py
@@ -0,0 +1,31 @@
+  # -*- coding = utf-8 -*-
+# Worker : HAN XIA
+# Motto : Practice makes perfect.
+# Time : 17/8/2023 8:12 pm
+
+import qrcode
+
+class FirstQR:
+    def __init__(self, size: int, padding: int ):
+        self.qr = qrcode.QRCode(box_size=size, border=padding)
+
+    def create_qr(self, file_name: str, fg: str, bg: str):
+        user_input:str = input('Enter the text:')
+
+        try:
+            self.qr.add_data(user_input)
+            qr_image = self.qr.make_image(fill_color=fg, back_color=bg)
+            qr_image.save(file_name)
+
+            print(f'Successfully created {file_name}! ')
+        except Exception as e :
+            print(f'Error: {e}')
+
+
+def main():
+    myqr = FirstQR(23,2)
+    myqr.create_qr('sample.png', 'black','yellow')
+
+if __name__ == '__main__':
+    main()
+
