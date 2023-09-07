90ed9805779f5e727878ae30f6d791e8661a9f1b
SummerDay <100746570+freesandwicha@users.noreply.github.com>
2023-09-07 14:50:53 +1000

    Create model.py

---
+++ Value_prediction_24/model.py
@@ -0,0 +1,17 @@
+# -*- coding = utf-8 -*-
+# Motto : Practice makes perfect.
+# Time : 7/9/2023 9:39 am
+
+#This program will predict the output based on the input from the user.
+from dataclasses import dataclass
+
+@dataclass
+class Prediction:
+    value: float
+    r2_score: float
+    slope: float
+    intercept: float
+    mean_absolute_error: float
+
+    def __str__(self):
+        return f'Prediction: {self.value:.2f} ({self.r2_score:.2%})'
